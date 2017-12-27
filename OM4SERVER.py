057111dc2ae5539b615b26572e8e879db1fffbcc
sunyue <sy@sunyue.sy>
2017-12-27 16:48:53 +0800

    解决编码问题

--- OM4SERVER.py
+++ OM4SERVER.py
@@ -2,14 +2,15 @@ from flask import Flask, redirect, request
 import requests
 import os
 import json
-import configparser
 
 app = Flask(__name__)
 
 @app.route('/')
 def read_json():
     r = requests.get('http://0.0.0.0:5000')
-    return r.content
+    s = json.loads(r.content.decode(encoding='utf8'), encoding='gbk')
+
+    return repr(s)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
